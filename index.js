5db861ad18c6726c2949ace3767190b446baaa3f
Marin <kuglermarinkevin@gmail.com>
2024-06-25 11:33:14 -0300

    salvando codigo incompleto

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 let consultas = []
-let novaConsulta = [
+let novaConsulta = 
     {paciente: "",
         medico: "",
         data: "",
         hora: ""
     }
-]
+let alterar
 
 console.log("Para marcar uma nova consulta, digite o nome do paciente:");
 
 process.stdin.on("data", function(data) {
     data = data.toString().trim();
+
+    if(data.toLowerCase() == "alterar" || data.toLowerCase() == "cancelar") {
+        switch (data.toLowerCase()) {
+            case "alterar":
+                
+                break;
+                case "cancelar":
+                    
+                    break;
+            default:
+                break;
+        }
+
+    }
+
+
     if (!novaConsulta.paciente) {
         novaConsulta.paciente = data;
         consultas.push("Paciente: " + novaConsulta.paciente);
@@ -26,16 +42,16 @@ process.stdin.on("data", function(data) {
     }else if(!novaConsulta.hora) {
         novaConsulta.hora = data;
         consultas.push("Horário: " + novaConsulta.hora);
-        novaConsulta = [
+        novaConsulta =
             {paciente: "",
                 medico: "",
                 data: "",
                 hora: ""
             }
-        ]
         console.log("Consulta agendada, confira abaixo:");
         console.log(consultas);
-        console.log("Digite o nome do próximo paciente:")
+        console.log("Digite o nome do próximo paciente, 'alterar' para alterar uma consulta ou 'cancelar' para cancelamentos: ")
+        
     }
 })
 
